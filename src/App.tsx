ac0ae03df57bd66bc41df7ba4067c1dc98622b4a
HuyNT <tienhuy031001@gmail.com>
2021-12-28 00:55:15 +0700

    oke

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GlobalStyles from "./components/GlobalStyles";
 import { MainHeader, MainContent, FormEdit } from "./pages/index.js";
 import { Route, Routes } from "react-router-dom";
@@ -7,7 +7,11 @@ import { ListDataStudent, Student } from "./pages/Data";
 function App() {
   const [studentList, setStudentList] =
     useState<Array<Student>>(ListDataStudent);
+    const [dataBackup, setDataBackup] =
+    useState<Array<Student>>(ListDataStudent);
   const [studentEdit, setStudentEdit] = useState<Student | null>(null);
+  const [filterSort, setFilterSort] = useState<string | number>("");
+  const [filterSearch, setFilterSearch] = useState<string>("");
 
   const handleUpdate = (id: number) => {
     const studentEditById = studentList.find((e: Student) => {
@@ -20,24 +24,60 @@ function App() {
 
   const handleDelete = (id: number): void => {
     setStudentList(() => {
-      return studentList.filter((e) => e.id !== id)
-    })
+      return studentList.filter((e) => e.id !== id);
+    });
   };
 
   const handleAdd = (student: Student): void => {
-    if (student?.id !== undefined){
+    if (student?.id !== undefined) {
       const studentListEdit = studentList.map((e) => {
-        if(e.id === student.id){
+        if (e.id === student.id) {
           return student;
         }
         return e;
-      })
+      });
       setStudentList(studentListEdit);
     } else {
       const idItem = studentList.slice(-1)[0].id + 1;
       student.id = idItem;
       setStudentList([...studentList, student]);
     }
+  };
+
+  useEffect(() => {
+    let sorted = studentList;
+    if (filterSearch.trim() === "all") {
+      console.log('all')
+      sorted = dataBackup;
+    } else if (filterSearch) {
+      setDataBackup(studentList);
+      sorted = sorted.filter((item: any) => {
+        if (
+          item.id === parseInt(filterSearch) ||
+          item.fullname === filterSearch ||
+          item.email === filterSearch ||
+          item.phone === filterSearch
+        ) {
+          return item;
+        }
+      });
+    }
+    if (filterSort) {
+      sorted = sorted.sort((a: any, b: any) => {
+        if (a[filterSort] > b[filterSort]) return 1;
+        else if (a[filterSort] < b[filterSort]) return -1;
+        return 0;
+      });
+    }
+    setStudentList([...sorted]);
+  }, [filterSort, filterSearch]);
+
+  const handleSort = (type: string | number) => {
+    setFilterSort(type);
+  };
+
+  const handleSearch = (input: string) => {
+    setFilterSearch(input.toLowerCase());
   };
 
   return (
@@ -53,11 +93,24 @@ function App() {
               studentList={studentList}
               handleDelete={handleDelete}
               handleUpdate={handleUpdate}
+              handleSort={handleSort}
+              handleSearch={handleSearch}
             />
           }
         ></Route>
-        <Route path="/add" element={<FormEdit handleProcess={handleAdd} studentEdit={null} />}></Route>
-        <Route path="/edit" element={<FormEdit handleProcess={handleAdd} studentEdit={studentEdit as Student} />}></Route>
+        <Route
+          path="/add"
+          element={<FormEdit handleProcess={handleAdd} studentEdit={null} />}
+        ></Route>
+        <Route
+          path="/edit"
+          element={
+            <FormEdit
+              handleProcess={handleAdd}
+              studentEdit={studentEdit as Student}
+            />
+          }
+        ></Route>
       </Routes>
     </>
   );
--- src/pages/MainContent/MainContent.tsx
+++ src/pages/MainContent/MainContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./MainContent.css";
 import { Student } from "../Data";
@@ -7,13 +7,63 @@ interface Props {
   studentList: Array<Student>;
   handleUpdate(id: number): void;
   handleDelete(id: number): void;
+  handleSearch: (value: string) => void;
+  handleSort: (type: string) => void;
 }
 
 export const MainContent: React.FC<Props> = (props) => {
+  //action search
+  const [valueInput, setValueInput] = useState("");
+
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (valueInput.trim()) {
+      props.handleSearch(valueInput);
+    }
+    setValueInput("");
+  };
+  const handleChangeInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValueInput(event.target.value);
+    // console.log(event.target.value);
+  };
+
+  //action sort
+  const [type, setType] = useState<string>("");
+  function handleSort(event: React.ChangeEvent<HTMLSelectElement>) {
+    const value = event.target.options[event.target.selectedIndex].value;
+    if (value) {
+      setType(value);
+      props.handleSort(value);
+    }
+  }
   return (
     <div className="content">
       <div className="container">
         <h1 className="title">List Students</h1>
+
+        {/* Select sort */}
+        <select onChange={handleSort}>
+          <option value="">Select field to sort</option>
+          <option value="id">Id</option>
+          <option value="fullname">fullname</option>
+          <option value="email">email</option>
+        </select>
+
+        {/* Select Search */}
+        <form action="" onSubmit={handleSearch}>
+          <div className="form-group">
+            <input
+              className="input"
+              value={valueInput}
+              type="text"
+              placeholder="Search...."
+              onChange={handleChangeInput}
+            />
+            <button type="submit" className="submit">
+              Search
+            </button>
+          </div>
+        </form>
         <table className="content-table-lists">
           <thead>
             <tr>
@@ -28,43 +78,51 @@ export const MainContent: React.FC<Props> = (props) => {
             </tr>
           </thead>
           <tbody>
-            {props.studentList.map((student, index) => {
-              return (
-                <tr key={index}>
-                  <td>{student.id}</td>
-                  <td>{student.fullname}</td>
-                  <td>{student.address ?? ""}</td>
-                  <td>{student.age ?? ""}</td>
-                  <td>{student.email}</td>
-                  <td>{student.phone}</td>
-                  <td>{student.gender ?? ""}</td>
-                  <td>
-                    <Link to="/edit" className="nav-link text-light">
-                      <button
-                        className="btn btn-edit"
-                        onClick={() => {
-                          props.handleUpdate(student.id);
-                        }}
-                      >
-                        Edit
-                      </button>
-                    </Link>
+            {props.studentList.length > 0 ? (
+              props.studentList.map((student, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{student.id}</td>
+                    <td>{student.fullname}</td>
+                    <td>{student.address ?? ""}</td>
+                    <td>{student.age ?? ""}</td>
+                    <td>{student.email}</td>
+                    <td>{student.phone}</td>
+                    <td>{student.gender ?? ""}</td>
+                    <td>
+                      <Link to="/edit" className="nav-link text-light">
+                        <button
+                          className="btn btn-edit"
+                          onClick={() => {
+                            props.handleUpdate(student.id);
+                          }}
+                        >
+                          Edit
+                        </button>
+                      </Link>
 
-                    <button
-                      className="btn btn-delete"
-                      onClick={() => props.handleDelete(student.id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
+                      <button
+                        className="btn btn-delete"
+                        onClick={() => props.handleDelete(student.id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan={3}>no users</td>
+              </tr>
+            )}
           </tbody>
         </table>
         <div>
           <Link to="/add" className="nav-link text-light">
-            <button type="button" className="btn btn-success">Create new students</button>
+            <button type="button" className="btn btn-success">
+              Create new students
+            </button>
           </Link>
         </div>
       </div>
